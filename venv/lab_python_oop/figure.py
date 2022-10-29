7d596565da2cd4398ef0e8c2441a6819e2b7a177
Khristina <hristinakirillova2559@gmail.com>
2022-10-30 00:53:45 +0300

    dVenv

--- venv/lab_python_oop/figure.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Figure(ABC):
-    @abstractmethod
-    def square(self):
-        """
-        содержит виртуальный метод для вычисления площади фигуры.
-        """
-        pass
