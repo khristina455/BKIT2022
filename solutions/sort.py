85b6bda0b84db5e2d1c885a8f7df6b559a667991
Khristina <hristinakirillova2559@gmail.com>
2022-12-18 18:01:17 +0300

    withoutSolutions

--- solutions/sort.py
+++
@@ -1,8 +0,0 @@
-data = [4, -30, 100, -100, 123, 1, 0, -1, -4]
-
-if __name__ == '__main__':
-    result = sorted(data, reverse = True, key = lambda x: abs(x))
-    print(result)
-
-    #result_with_lambda = ...
-    #print(result_with_lambda)
